d20d1fee7e1ae7a40f79443184f6d15212f5ec59
Taha Jalil <taha.jalil@gmail.com>
2014-04-22 16:22:41 -0400

    added actions to reservation controller

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,18 +1,34 @@
 class ReservationsController < ApplicationController
 
+  before_action :load_user, only: [:new, :create]
+
   def show
+    @reservation = Reservation.find(params[:id])
+  end
+
+  def new
+    @reservation = Reservation.new
   end
 
   def create
+    @reservation = @user.reservations.build(reservation_params)
+    @reservation.user_id = current_user.id
+
+    if @reservation.save
+      redirect_to user_show(@reservation.user_id), notice: "Successfully created your reservation"
+    else
+      render :new
+    end
   end
 
-  def edit
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:reservation_time, :party_size, :restaurant_id)
   end
 
-  def update
-  end
-
-  def create
+  def load_user
+    @user = User.find(params[:user_id])
   end
 
 end
